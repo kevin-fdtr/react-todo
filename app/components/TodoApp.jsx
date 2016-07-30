909475f9a246aa2977c836dea90f0d80ee675f39
Kevin Sorensen <kevin@fdtr.ca>
2016-07-30 15:52:07 -0600

    add TodoApp component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var TodoApp = React.createClass( {
+  render: function() {
+    return (
+      <div>
+        Todo App
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
